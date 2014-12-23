2d7e0d3ae2e4e101c30a99d18c94bffe6ce0d2ee
Will Duquette <will@wjduquette.com>
2014-12-23 08:18:15 -0800

    Simplified md2html for use with mutest.
    Expects a command that takes a filename and writes XHTML to stdout.

--- TODO.md
+++ TODO.md
@@ -1,8 +1,12 @@
 # TODO.md
 
+* Test against standard test suites: mdtest, commonmark
+* mdtest Results
+  * "Links, Reference Style" FAILED
+    * Tcl error thrown in parser
+  * "Ordered and unordered lists" seems to hang indefinitely.
 * Clean up coding style
 * Document specific-dialect of Markdown.
-* Test against standard test suites.
 * Identify gaps
 * Added capabilities?
   * regexp detection of special link syntax (i.e., #num for issue numbers
--- lib/app_md2html/main.tcl
+++ lib/app_md2html/main.tcl
@@ -18,23 +18,12 @@
 # Dummy procedure
 
 proc main {argv} {
-    puts "[quillinfo project] [quillinfo version]"
-    puts ""
-
     if {[llength $argv] == 0} {
         return -code error -errorcode FATAL "No documents to process"
     }
 
     foreach infile $argv {
-        puts $infile
-        if {[file extension $infile] ne ".md"} {
-            puts "$infile: Not a markdown file; skipping"
-            continue
-        }
         set mdtext [readfile $infile]
-
-        set outfile [file rootname $infile].html
-
-        writefile $outfile [markdown convert $mdtext]
+        puts [markdown convert $mdtext]
     }
 }
